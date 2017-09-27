d971a4d5d07b3e5adbb417ba2c7f1121fb54ac04
Niels van der Veer <n.i.vdveer@gmail.com>
2017-09-27 08:53:49 +0200

    Solve unique key warnings

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -15,10 +15,10 @@ let inner_login = function <U, R>(role_to_string: (role: R) => string) : (roles:
                 show_password ?
                     retract<AuthState<U, R>, string>('inner_login_password')(authState => authState.loginState.password, authState => v => ({ ...authState, loginState: {...authState.loginState, password: v}}),
                         label<string, string>("Password", true)(string("edit", "password", "password_input")))
-                    : authState => unit(null).never(),
+                    : authState => unit(null).never("inner_login_password"),
 
                 retract<AuthState<U, R>, R>('inner_login_role')(authState => authState.loginState.role, authState => v => ({ ...authState, loginState: {...authState.loginState, role: v}}),
-                    label<R, R>("Role", true)(r => selector<R>("dropdown", role_to_string)(roles, r))),
+                    label<R, R>("Role", true)(r => selector<R>("dropdown", role_to_string, "role_selector")(roles, r))),
             ])
         )
 }
@@ -32,7 +32,7 @@ let login = function <U, R>(loginApi: (loginData: LoginData<R>) => C<Option<U>>,
                         u.kind == "none" ? messageHandler("login_failed") : ""
                         return unit<AuthState<U, R>>({...ld, user: u})
                     })),
-            ld => a<AuthState<U, R>>("Forgot password?")({ ...ld, kind: "requestreset"})
+            ld => a<AuthState<U, R>>("Forgot password?", null, null, false,  "forgot_password")({ ...ld, kind: "requestreset"})
         ])
 }
 
@@ -40,26 +40,26 @@ let resetPasswordRequest = function <U, R>(resetApi: (loginData: LoginData<R>) =
     return (role_to_string: (role: R) => string) => (roles: R[]) =>
         any<AuthState<U, R>, AuthState<U, R>>('requestreset_form')([
             inner_login<U, R>(role_to_string)(roles)(false),
-            ld => button<AuthState<U, R>>("Request reset", false)(ld).then(undefined, ld =>
+            ld => button<AuthState<U, R>>("Request reset", false, "request_reset_button")(ld).then(undefined, ld =>
                 resetApi(ld.loginState).then(undefined, result => {
                     result == "success" ? messageHandler("reset_success") : messageHandler("reset_failed")
                     return unit<AuthState<U, R>>(ld)
                 })
             ),
-            ld => a<AuthState<U, R>>("Back to login")({ ...ld, kind: "login"})
+            ld => a<AuthState<U, R>>("Back to login", null, null, false, "back_to_login")({ ...ld, kind: "login"})
         ])
 }
 
 let logout = function <U, R>(logoutApi: (loginData: LoginData<R>) => C<void>, messageHandler: (message: string) => void) : (_: AuthState<U, R>) => C<AuthState<U, R>> {
-       return any<AuthState<U, R>, AuthState<U, R>>()([
-            ld => button<AuthState<U, R>>("Logout", false)(ld).then(undefined, ld => logoutApi(ld.loginState)).then(undefined, _ => unit<AuthState<U, R>>({...ld, kind: "login"}))
+       return any<AuthState<U, R>, AuthState<U, R>>('logout_form')([
+            ld => button<AuthState<U, R>>("Logout", false, "logout_button")(ld).then(undefined, ld => logoutApi(ld.loginState)).then(undefined, _ => unit<AuthState<U, R>>({...ld, kind: "login"}))
         ])
 }
 
 export let Authenticate = function <U, R>(loginApi: (loginData: LoginData<R>) => C<Option<U>>, logoutApi: (loginData: LoginData<R>) => C<void>, resetApi: (loginData: LoginData<R>) => C<ResetPasswordResult>, messageHandler: (message: string) => void) {
     return (role_to_string: (role: R) => string) => (roles: R[]) => (authState: AuthState<U, R>): C<Option<U>> =>
-        repeat<AuthState<U, R>>()(
-            any<AuthState<U, R>, AuthState<U, R>>()([
+        repeat<AuthState<U, R>>('authenticate')(
+            any<AuthState<U, R>, AuthState<U, R>>('authenticate_wrapper')([
                 ld =>
                     ld.kind == "login" ?
                         login<U, R>(loginApi, messageHandler)(role_to_string)(roles)(ld)
