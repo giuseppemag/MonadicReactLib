bba877ecf9b39eea168dbb291b94a1de90f07b3a
Niels van der Veer <n.i.vdveer@gmail.com>
2017-09-28 11:10:00 +0200

    Improve and add additional keys; Return selector role as well

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -4,21 +4,22 @@ export type LoginData<R> = { email: string, password: string, role: R }
 export type ResetPasswordData<R> = { email: string, role: R }
 export type ResetPasswordResult = "success" | "failure"
 export type AuthState<U, R> = { kind: "login" | "requestreset" | "logout", loginState: LoginData<R>, user: Option<U> }
+export type AuthResult<U, R> = {role: R, user: Option<U>}
 
 let inner_login = function <U, R>(role_to_string: (role: R) => string) : (roles: R[]) => (show_password: boolean) => (_: AuthState<U, R>) => C<AuthState<U, R>> {
     return (roles: R[]) => (show_password: boolean) =>
-        repeat<AuthState<U, R>>("inner_login")(
-            any<AuthState<U, R>, AuthState<U, R>>()([
-                retract<AuthState<U, R>, string>("inner_login_email")(authState => authState.loginState.email, authState => v => ({ ...authState, loginState: {...authState.loginState, email: v}}),
-                    label<string, string>("Email", true)(string("edit", "email", "email_input"))),
+        repeat<AuthState<U, R>>("inner_login_repeat")(
+            any<AuthState<U, R>, AuthState<U, R>>("inner_login_any")([
+                retract<AuthState<U, R>, string>("email_retract")(authState => authState.loginState.email, authState => v => ({ ...authState, loginState: {...authState.loginState, email: v}}),
+                    label<string, string>("Email", true, null, "email_label")(string("edit", "email", "email_input"))),
 
                 show_password ?
-                    retract<AuthState<U, R>, string>('inner_login_password')(authState => authState.loginState.password, authState => v => ({ ...authState, loginState: {...authState.loginState, password: v}}),
-                        label<string, string>("Password", true)(string("edit", "password", "password_input")))
+                    retract<AuthState<U, R>, string>('password_retract')(authState => authState.loginState.password, authState => v => ({ ...authState, loginState: {...authState.loginState, password: v}}),
+                        label<string, string>("Password", true, null, "password_label")(string("edit", "password", "password_input")))
                     : authState => unit(null).never("inner_login_password"),
 
-                retract<AuthState<U, R>, R>('inner_login_role')(authState => authState.loginState.role, authState => v => ({ ...authState, loginState: {...authState.loginState, role: v}}),
-                    label<R, R>("Role", true)(r => selector<R>("dropdown", role_to_string, "role_selector")(roles, r))),
+                retract<AuthState<U, R>, R>('role_retract')(authState => authState.loginState.role, authState => v => ({ ...authState, loginState: {...authState.loginState, role: v}}),
+                    label<R, R>("Role", true, null, "role_label")(r => selector<R>("dropdown", role_to_string, "role_selector")(roles, r))),
             ])
         )
 }
@@ -57,7 +58,7 @@ let logout = function <U, R>(logoutApi: (loginData: LoginData<R>) => C<void>, me
 }
 
 export let Authenticate = function <U, R>(loginApi: (loginData: LoginData<R>) => C<Option<U>>, logoutApi: (loginData: LoginData<R>) => C<void>, resetApi: (loginData: LoginData<R>) => C<ResetPasswordResult>, messageHandler: (message: string) => void) {
-    return (role_to_string: (role: R) => string) => (roles: R[]) => (authState: AuthState<U, R>): C<Option<U>> =>
+    return (role_to_string: (role: R) => string) => (roles: R[]) => (authState: AuthState<U, R>): C<AuthResult<U, R>> =>
         repeat<AuthState<U, R>>('authenticate')(
             any<AuthState<U, R>, AuthState<U, R>>('authenticate_wrapper')([
                 ld =>
@@ -68,5 +69,5 @@ export let Authenticate = function <U, R>(loginApi: (loginData: LoginData<R>) =>
                     :
                         resetPasswordRequest<U, R>(resetApi, messageHandler)(role_to_string)(roles)(ld)
             ])
-        )(authState).filter(s => s.kind != "requestreset" && s.kind != "logout").map((f) => f.user)
+        )(authState).filter(s => s.kind != "requestreset" && s.kind != "logout").map((f) => {return {role: f.loginState.role, user: f.user}})
 }
