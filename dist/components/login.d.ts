eff6f4c90db96615645c9f152a8ba4cbdaaf2e9d
Niels van der Veer <n.i.vdveer@gmail.com>
2017-09-28 11:12:38 +0200

    Compile new version

--- dist/components/login.d.ts
+++ dist/components/login.d.ts
@@ -14,4 +14,8 @@ export declare type AuthState<U, R> = {
     loginState: LoginData<R>;
     user: Option<U>;
 };
-export declare let Authenticate: <U, R>(loginApi: (loginData: LoginData<R>) => C<Option<U>>, logoutApi: (loginData: LoginData<R>) => C<void>, resetApi: (loginData: LoginData<R>) => C<ResetPasswordResult>, messageHandler: (message: string) => void) => (role_to_string: (role: R) => string) => (roles: R[]) => (authState: AuthState<U, R>) => C<Option<U>>;
+export declare type AuthResult<U, R> = {
+    role: R;
+    user: Option<U>;
+};
+export declare let Authenticate: <U, R>(loginApi: (loginData: LoginData<R>) => C<Option<U>>, logoutApi: (loginData: LoginData<R>) => C<void>, resetApi: (loginData: LoginData<R>) => C<ResetPasswordResult>, messageHandler: (message: string) => void) => (role_to_string: (role: R) => string) => (roles: R[]) => (authState: AuthState<U, R>) => C<AuthResult<U, R>>;
